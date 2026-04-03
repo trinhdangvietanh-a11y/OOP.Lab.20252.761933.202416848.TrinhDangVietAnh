d7157b0fa4ee162ec1aa89bb627e30d3c3ccdf4b
trinhdangvietanh-a11y <trinhdangvietanh@thptchuyenlamson.edu.vn>
2026-04-03 23:54:29 +0700

    Upload Lab2

---
+++ lab2/Design/lab2_aims/Aims.java
@@ -0,0 +1,34 @@
+package lab2_aims;
+
+public class Aims {
+    public static void main(String[] args) {
+        Cart anOrder = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("Simba", 
+                "Animation", "Roger Steve", 87, 20f);
+        anOrder.addDigitalVideoDisc(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Fall", 
+                "Science Fiction", "George II", 87, 20f);
+        anOrder.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Alibaba", 
+                "Animation", "Aladin", 87, 20.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+        
+        System.out.println("Total Cost is: ");
+        System.out.println(anOrder.totalCost());
+        
+        //11
+        anOrder.removeDigitalVideoDisc(dvd2);
+     
+        System.out.println("Total Cost after removing Star FALL is: ");
+        System.out.println(anOrder.totalCost());
+        
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("HIHI", "Sci-Fi", "HAHA", 100, 20.0f);
+        anOrder.removeDigitalVideoDisc(dvd4);
+
+        System.out.println("Total Cost is: ");
+        System.out.println(anOrder.totalCost());
+    }
+}
